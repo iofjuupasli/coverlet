c4e3bf73bec210485a96495ea852db10bffe80af
iofjuupasli <iofjuupasli@gmail.com>
2014-03-31 17:45:28 +0300

    testing preparation

---
+++ test/RestSpec.js
@@ -0,0 +1,9 @@
+/*global describe, it, before, beforeEach, after, afterEach*/
+/*global coverlet*/
+
+describe('Rest', function () {
+    'use strict';
+    it('should exist', function () {
+        coverlet.Rest.should.exist;
+    });
+});
